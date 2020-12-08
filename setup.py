6770e772e4d558efd6d3e518daf5fd443fa792f7
Deepanshu Bagotia <61933230+Bagotia16@users.noreply.github.com>
2020-12-08 21:36:41 +0530

    Update setup.py
    description added

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
 setup(
     name="chass",
-    version='1.0',
+    version='0.0.1',
+    authon="chass team",
+    description="A user friendly CLI Debugging application exclusively for Bash Scripts",
+    long_description = long_description,
+    long_description_content_type="markdown",
+    url="https://github.com/aadishjain2911/CHASS",
     packages=find_packages(),
     py_modules=['chass'],
+    classifiers=[
+    	"Programming Language :: Python :: 3",
+    	"License :: OSI Approved :: MIT License",
+    	"Operating System :: OS Independent",
+    ],
     install_requires=[
         'Click',
     ],
